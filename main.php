6136c100c8dadac953e2d9b030a94d64f2c7d81e
W4RR10R <46273006+CW4RR10R@users.noreply.github.com>
2020-10-30 10:58:40 +0530

    ...

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+// usage main.php?c=0-9-129
+$input = $_GET["c"];
+if (!$input){
+   exit("Channel ID not found");
+}
+$channel_meta = JsonfromURI("https://catalogapi.zee5.com/v1/channel/${input}");
+$stream_url = $channel_meta->stream_url_hls;
+$tok_json = JsonfromURI("https://useraction.zee5.com/token/live.php");
+$video_token = $tok_json->video_token;
+$m3u8 = $stream_url.$video_token;
+echo $m3u8;
+
+function JsonfromURI($url) {
+   $resp = file_get_contents($url);       
+   return json_decode($resp);
+};
